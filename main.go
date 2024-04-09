76b0733ec2ca29f06d8a517d519c8d17ebb02c31
HLTienDat <hltiendat2002@gmail.com>
2024-04-09 13:12:10 +0700

    Add API CRUD

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	// "html"
-	// "net/http"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
@@ -15,104 +19,37 @@ const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
-	password = "0125661"
+	password = "091372"
 	dbname   = "Users"
 )
 
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
-
-	// http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hi")
-	// })
-
-	// log.Fatal(http.ListenAndServe(":8081", nil))
-	// Establish a connection to the PostgreSQL database
-	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
-	if err != nil {
-		log.Fatal("Error connecting to the database: ", err)
-	}
-	defer db.Close()
-	ts := time.Now()
-
-	idx, err := CreateEmployee(db, "Hoang Le Tien Dat", ts, "hltiendat2002@gmail.com", "0943211621", "274981446", "Nguyen Thi Minh Khai")
-	if err != nil {
-		log.Fatal("Error creating employee: ", err)
-	}
-	fmt.Println("Created employee with ID:", idx)
-	idy, err := CreateAccount(db, "tiendathola", "matkhauDonGian", idx, 1)
-	if err != nil {
-		log.Fatal("Error creating account: ", err)
-	}
-	fmt.Println("Created account with ID:", idy)
-
-	e, err := ReadEmployee(db, idx)
-	if err != nil {
-		log.Fatal("Error reading records: ", err)
-	}
-	fmt.Println("===Employee===")
-	fmt.Println("ID:", e.id)
-	fmt.Println("Name:", e.name)
-	fmt.Println("Email:", e.email)
-	fmt.Println("Address:", e.address)
-	fmt.Println("Phone:", e.phone)
-
-	err = UpdateEmployee(db, idx, "Hoang Le Tien Dinh", ts, "susieglitter@rhodes.com", "0943911616", "255", "Sparkle .JR Caster Rhodes")
-	if err != nil {
-		log.Fatal("Error updating employee: ", err)
-	}
-	fmt.Println("Updated employee with ID:", idx)
-	e1, err := ReadEmployee(db, idx)
-	if err != nil {
-		log.Fatal("Error reading employee: ", err)
-	}
-	fmt.Println("Employee Ater Updating:")
-	fmt.Println("===Employee===")
-	fmt.Println("ID:", e1.id)
-	fmt.Println("Name:", e1.name)
-	fmt.Println("Email:", e1.email)
-	fmt.Println("Address:", e1.address)
-	fmt.Println("Phone:", e1.phone)
-
-	err = DeleteRecord(db, 1)
-	if err != nil {
-		log.Fatal("Error deleting record: ", err)
-	}
-	fmt.Println("Deleted record with ID:", 1)
-	records, err := ReadAllEmployee(db)
-	if err != nil {
-		log.Fatal("Error reading records: ", err)
-	}
-	fmt.Println("All records:")
-	for _, e2 := range records {
-		fmt.Println("===Employee===")
-		fmt.Println("ID:", e2.id)
-		fmt.Println("Name:", e2.name)
-		fmt.Println("Email:", e2.email)
-		fmt.Println("Address:", e2.address)
-		fmt.Println("Phone:", e2.phone)
-	}
+	router := gin.Default()
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getSpecificUser)
+	router.POST("/users", postUsers)
+	router.DELETE("/users/:id", delUsers)
+	router.PUT("/users/:id", updateUser)
+	router.Run("localhost:8080")
 
 }
 
 type Account struct {
-	id         int
-	username   string
-	password   string
-	employeeId string
-	role       int
+	Id         int    `json:"id"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	EmployeeId string `json:"employeeId"`
+	Role       int    `json:"role"`
 }
 
 type Employee struct {
-	id        int
-	name      string
-	dob       time.Time
-	email     string
-	phone     string
-	citizenId string
-	address   string
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Dob       time.Time `json:"dob"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	CitizenId string    `json:"citizenId"`
+	Address   string    `json:"address"`
 }
 
 func CreateEmployee(db *sql.DB, name string, dob time.Time, email string, phone string, citizenId string, address string) (int, error) {
@@ -136,7 +73,7 @@ func ReadEmployee(db *sql.DB, id int) (Employee, error) {
 	var e Employee
 	row := db.QueryRow("SELECT id, name,dob, email,phone,citizenId,address FROM Employee WHERE id=$1", id)
 
-	err := row.Scan(&e.id, &e.name, &e.dob, &e.email, &e.phone, &e.citizenId, &e.address)
+	err := row.Scan(&e.Id, &e.Name, &e.Dob, &e.Email, &e.Phone, &e.CitizenId, &e.Address)
 	if err != nil {
 		return e, err
 	}
@@ -149,34 +86,133 @@ func ReadAllEmployee(db *sql.DB) ([]Employee, error) {
 	}
 	defer rows.Close()
 
-	var records []Employee
+	var employees []Employee
 	for rows.Next() {
 		var e Employee
-		err := rows.Scan(&e.id, &e.name, &e.dob, &e.email, &e.phone, &e.citizenId, &e.address)
+		err := rows.Scan(&e.Id, &e.Name, &e.Dob, &e.Email, &e.Phone, &e.CitizenId, &e.Address)
 		if err != nil {
 			return nil, err
 		}
-		records = append(records, e)
+		employees = append(employees, e)
 	}
-	return records, nil
+	return employees, nil
 }
 
-func UpdateEmployee(db *sql.DB, id int, name string, dob time.Time, email string, phone string, citizenId string, address string) error {
+func UpdateEmployee(db *sql.DB, id int, name string, dob time.Time, email string, phone string, citizenId string, address string) (int, error) {
 	_, err := db.Exec("UPDATE Employee SET name=$1, dob=$2, email=$3, phone=$4, citizenId=$5, address=$6 WHERE id=$7", name, dob, email, phone, citizenId, address, id)
 	if err != nil {
-		return err
+		return id, err
 	}
-	return nil
+	return id, nil
 }
 
-func DeleteRecord(db *sql.DB, id int) error {
+func DeleteUser(db *sql.DB, id int) (int, error) {
 	_, err1 := db.Exec("DELETE FROM ACCOUNT WHERE id=$1", id)
 	if err1 != nil {
-		return err1
+		return -1, err1
 	}
 	_, err := db.Exec("DELETE FROM EMPLOYEE WHERE id=$1", id)
 	if err != nil {
-		return err
+		return -1, err
 	}
-	return nil
+	return id, nil
+}
+func getUsers(c *gin.Context) {
+
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+	employees, err := ReadAllEmployee(db)
+	if err != nil {
+		log.Fatal("Error reading database: ", err)
+	}
+	c.IndentedJSON(http.StatusOK, employees)
+}
+func getSpecificUser(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "id does not valid"})
+		return
+	}
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+	employee, err := ReadEmployee(db, id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "something wrong happen"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, employee)
+}
+func postUsers(c *gin.Context) {
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+	var newEmployee Employee
+	if err := c.BindJSON(&newEmployee); err != nil {
+		return
+	}
+	i, err := CreateEmployee(db, newEmployee.Name, newEmployee.Dob, newEmployee.Email, newEmployee.Phone, newEmployee.CitizenId, newEmployee.Address)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Printf("Employee %v has been created\n", i)
+	i1, err := CreateAccount(db, newEmployee.Email, newEmployee.CitizenId, i, 1)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Printf("Account %v has been created \n", i1)
+	c.IndentedJSON(http.StatusCreated, newEmployee)
+}
+
+func updateUser(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Id is not valid"})
+		return
+	}
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+	var newEmployee Employee
+	if err := c.BindJSON(&newEmployee); err != nil {
+		return
+	}
+	i, err := UpdateEmployee(db, id, newEmployee.Name, newEmployee.Dob, newEmployee.Email, newEmployee.Phone, newEmployee.CitizenId, newEmployee.Address)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Printf("Employee %v has been updated\n", i)
+
+	c.IndentedJSON(http.StatusCreated, newEmployee)
+}
+
+func delUsers(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Id is not valid"})
+		return
+	}
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+	i, err := DeleteUser(db, id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"message": fmt.Sprintf("User %v has been deleted", i)})
 }
